b1946e7853dc19f66811c38c8973787b65b81835
Dusan <dusan@adlede.com>
2023-12-16 11:16:19 +0100

    Make password_hash longer to 256

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
