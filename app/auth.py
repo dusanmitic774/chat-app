ce1daf9f85ec3f1e2081fc06d72f3735995d9701
Dusan <dusan@adlede.com>
2024-01-27 18:40:45 +0100

    Force strong password

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask import session
 from flask_login import login_user, logout_user
@@ -7,6 +9,20 @@ from app.database import db
 from app.models import User
 
 auth = Blueprint("auth", __name__)
+
+
+def is_password_strong(password):
+    if len(password) < 8:
+        return False, "Password must be at least 8 characters long."
+    if not re.search("[a-z]", password):
+        return False, "Password must contain at least one lowercase letter."
+    if not re.search("[A-Z]", password):
+        return False, "Password must contain at least one uppercase letter."
+    if not re.search("[0-9]", password):
+        return False, "Password must contain at least one number."
+    if not re.search('[!@#$%^&*(),.?":{}|<>]', password):
+        return False, "Password must contain at least one special character."
+    return True, "Password is strong."
 
 
 @auth.route("/logout")
@@ -22,20 +38,24 @@ def signup():
         email = request.form.get("email")
         password = request.form.get("password")
 
+        is_strong, message = is_password_strong(password)
+        if not is_strong:
+            flash(message, "error")
+            return redirect(url_for("auth.signup", show="signup"))
+
         user = User.query.filter(
             or_(User.username == username, User.email == email)
         ).first()
 
         if user:
             flash("User already exists", "error")
-            return redirect(url_for("auth.signup"))
+            return redirect(url_for("auth.signup", show="signup"))
 
         new_user = User(username=username, email=email)
         new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
 
-        flash("Signup successful!")
         return redirect(url_for("chat.chat"))
 
     return render_template("auth.html")
@@ -57,7 +77,6 @@ def login():
 
         login_user(user)
         session.permanent = True
-        flash("Login successful!")
 
         return redirect(url_for("chat.chat"))
 
